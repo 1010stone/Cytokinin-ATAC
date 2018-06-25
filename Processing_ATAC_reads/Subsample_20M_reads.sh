73c628e08dfda7c7e0db1e64e16f9a2dd33b06cb
Kevin Potter <potterkc@ad.unc.edu>
2018-06-25 14:15:18 -0400

    Create Subsample_20M_reads.sh

---
+++ Processing_ATAC_reads/Subsample_20M_reads.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+# Script to subsample 20 million reads from each BAM file and index them.
+
+# Load in modules
+module load samtools bbmap bedtools
+
+# Create variable containing file names.
+ATAC=(ATAC_1 ATAC_2 ATAC_n)
+
+# Create variable containing number of reads to subsample.
+READS="20000000"
+
+# Create variable that contains the total number of reads for each dataset.
+declare -a total=()
+
+for i in "${!ATAC[@]}"
+do
+	total+=( "$(samtools view -c -F 260 BAM_FILES/${ATAC[i]}_SHIFTED_SORTED.bam)" )
+	echo ${arr[*]}
+done
+
+declare -p total
+
+
+#Calculates the normalization factor to get to 20M reads per library.
+declare -a factor=()
+
+
+for i in "${!total[@]}"
+do
+	factor+=($(bc <<< "scale=2; $READS/${total[i]}"))
+	echo ${factor[*]}
+done
+
+declare -p factor
+
+
+for i in ${!ATAC[@]}
+do
+	samtools view -s ${factor[i]} -b BAM_FILES/${ATAC[i]}_SHIFTED_SORTED.bam >BAM_FILES/${ATAC[i]}_SUBSAMPLED.bam" \
+	&& samtools index BAM_FILES/${ATAC[i]}_SUBSAMPLED.bam
+done
