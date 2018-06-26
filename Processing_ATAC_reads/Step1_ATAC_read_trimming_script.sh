6b268a1f554d99adf2e181aaff6ed3535390e825
Kevin Potter <potterkc@ad.unc.edu>
2018-06-26 10:36:31 -0400

    Update Step1_ATAC_read_trimming_script.sh

--- Processing_ATAC_reads/Step1_ATAC_read_trimming_script.sh
+++ Processing_ATAC_reads/Step1_ATAC_read_trimming_script.sh
@@ -7,21 +7,23 @@ module load bbmap
 
 # Determine the bbmap version number \
 # this is needed to find the correct directory \
-# containing the updated Arabidopsis index
+# containing the updated Arabidopsis index \
+# for correct version of bbmap (it gets updated frequently...)
 bbmap=`echo $LOADEDMODULES| awk -F'bbmap/' '{print \$2}'`
 
 # Create variable for unique directory names that correspond to different library names.
 NAMES=(BAM_1 BAM_2 BAM_n)
 
-ATAC=(ATAC_1_CGAGGCTG_S1_L008 ATAC_2_AAGAGGCA_S2_L008 ATAC_3_TAAGGCGA_S3_L008 ATAC_4_CGTACTAG_S4_L008)
+ATAC=(ATAC_1 ATAC_2 ATAC_n)
 
 for i in ${!ATAC[@]}
 do
-  bbduk.sh in=../../../HTSF/161202_UNC32-K00270_0031_BHCKF2BBXX/${ATAC[i]}_R1_001.fastq.gz \
-    in2=../../../HTSF/161202_UNC32-K00270_0031_BHCKF2BBXX/${ATAC[i]}_R2_001.fastq.gz \
-    out=FASTQ_FILES/${NAMES[i]}-R1.trimmed.fastq \
-    out2=FASTQ_FILES/${NAMES[i]}-R2.trimmed.fastq \
-    ref=/nas02/apps/bbmap-$bbmap/bbmap/resources/adapters.fa \
-    k=13 \
-    ktrim=r
+  bbduk.sh \
+    in=${ATAC[i]}_R1_001.fastq.gz \ # input read pair 1
+    in2=${ATAC[i]}_R2_001.fastq.gz \ # input read pair 2
+    out=${NAMES[i]}-R1.trimmed.fastq \ # output trimmed reads for read pair 1
+    out2=${NAMES[i]}-R2.trimmed.fastq \ # output trimmed reads for read pair 2
+    ref=/nas02/apps/bbmap-$bbmap/bbmap/resources/adapters.fa \ # file containing Illumina adapter sequences
+    k=13 \ # kmers to trim
+    ktrim=r # direction of ktrimming
 done
