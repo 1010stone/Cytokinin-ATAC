7bf9d7a502232a8916bc72e1cb7eecf8ce48f19a
Kevin Potter <potterkc@ad.unc.edu>
2018-06-26 10:20:06 -0400

    Update Step5_Counting_reads_in_differential_regions.sh

--- Peak_calling_and_differential_analysis/Step5_Counting_reads_in_differential_regions.sh
+++ Peak_calling_and_differential_analysis/Step5_Counting_reads_in_differential_regions.sh
@@ -2,4 +2,10 @@
 
 # Script to count reads within differential regions and plot averaged read densities.
 
+module load bedtools
 
+bedtools multicov \
+  -bams ATAC_merged_treated_datasets.bam \
+        ATAC_merged_control_datasets.bam \
+  -bed Differential_regions.bed \
+  >matrix.txt
