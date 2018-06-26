e300f23d772adb5a19c7edae07306bc6d967e24f
Kevin Potter <potterkc@ad.unc.edu>
2018-06-26 10:32:33 -0400

    Update Step1_Shift_reads.sh

--- Peak_calling_and_differential_analysis/Step1_Shift_reads.sh
+++ Peak_calling_and_differential_analysis/Step1_Shift_reads.sh
@@ -2,21 +2,26 @@
 
 # Script to shift reads +4 on forward and -5.
 
-ATAC="ATAC_1 ATAC_2 ATAC_n"
-
+# Load in modules
 module load samtools bedtools
+
+# Create variable containing unique descriptors of your datasets.
+ATAC="ATAC_1 ATAC_2 ATAC_n"
 
 for ATAC in $ATAC
   do
     awk 'BEGIN {OFS ="\t"} {if ($6 == "+"){print $1, $2 + 4, $3 + 4, $4, $5, $6} \
       else{print $1, $2 - 5, $3 - 5, $4, $5, $6}}' \
-      BED_FILES/$ATAC.bed >BED_FILES/$ATAC_SHIFTED_UNSORTED.bed \
+      $ATAC.bed >$ATAC_SHIFTED_UNSORTED.bed \
     
-    && sort -k1,1 -k2,2n -k3,3n BED_FILES/$ATAC_SHIFTED_UNSORTED.bed \
-      >BED_FILES/$ATAC_SHIFTED_SORTED.bed \
+    # now sort resulting bed file
+    && sort -k1,1 -k2,2n -k3,3n $ATAC_SHIFTED_UNSORTED.bed \
+      >$ATAC_SHIFTED_SORTED.bed \
     
-    && bedtools bedtobam -i BED_FILES/$ATAC_SHIFTED_SORTED.bed \
-      -g genome.txt >BAM_FILES/$ATAC_SHIFTED_SORTED.bam \
+    # convert bed file back to bam file
+    && bedtools bedtobam -i $ATAC_SHIFTED_SORTED.bed \
+      -g genome.txt >$ATAC_SHIFTED_SORTED.bam \
     
-    && samtools index BAM_FILES/$ATAC_SHIFTED_SORTED.bam
+    # index new bam containing shifted and sorted reads
+    && samtools index $ATAC_SHIFTED_SORTED.bam
   done
