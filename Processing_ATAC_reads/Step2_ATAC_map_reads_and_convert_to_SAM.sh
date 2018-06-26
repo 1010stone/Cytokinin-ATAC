4d68da64fd001ae853c82cd9311dfd5d6e8ead1b
Kevin Potter <potterkc@ad.unc.edu>
2018-06-26 10:38:38 -0400

    Update Step2_ATAC_map_reads_and_convert_to_SAM.sh

--- Processing_ATAC_reads/Step2_ATAC_map_reads_and_convert_to_SAM.sh
+++ Processing_ATAC_reads/Step2_ATAC_map_reads_and_convert_to_SAM.sh
@@ -11,11 +11,15 @@ NAMES="ATAC_1 ATAC_2 ATAC_n"
 for ATAC in $ATAC
   do
     bowtie -X 1000 -t -p 4 -v 2 --best --strata -m 2 -y -k 1 \
-      /proj/seq/data/TAIR10_Ensembl/Sequence/BowtieIndex/genome \ 
-      -1 FASTQ_FILES/$ATAC-R1.trimmed.fastq \ 
-      -2 FASTQ_FILES/$ATAC-R2.trimmed.fastq \ 
-      -S SAM_FILES/$ATAC_MAPPED.sam \
-    && samtools view -b SAM_FILES/$ATAC_MAPPED.sam \ 
-      |samtools sort - -o BAM_FILES/$ATAC_MAPPED_SORTED.bam \
-    && samtools index BAM_FILES/$POTTER_MAPPED_SORTED.bam
+      /proj/seq/data/TAIR10_Ensembl/Sequence/BowtieIndex/genome \ # location of bowtie index for genome
+      -1 $ATAC-R1.trimmed.fastq \ # input trimmed reads for read pair 1
+      -2 $ATAC-R2.trimmed.fastq \ # input trimmed reads for read pair 2
+      -S $ATAC_MAPPED.sam \ # output mapped reads in sam format
+   
+    # convert sam to bam
+    && samtools view -b $ATAC_MAPPED.sam \ 
+      |samtools sort - -o $ATAC_MAPPED_SORTED.bam \
+    
+    # index bam file
+    && samtools index $ATAC_MAPPED_SORTED.bam
   done
