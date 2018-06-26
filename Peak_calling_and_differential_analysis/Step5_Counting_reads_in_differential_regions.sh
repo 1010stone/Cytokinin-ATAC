b0229bc9781b0f59c092292c1aa220b08c9dab85
Kevin Potter <potterkc@ad.unc.edu>
2018-06-25 20:37:19 -0400

    Update Step5_Counting_reads_in_differential_regions.sh

--- Peak_calling_and_differential_analysis/Step5_Counting_reads_in_differential_regions.sh
+++ Peak_calling_and_differential_analysis/Step5_Counting_reads_in_differential_regions.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -l
 
-# Script to count reads within differential regions.
+# Script to count reads within differential regions and plot averaged read densities.
 
-module load samtools bedtools
 
-bedtools multicov \
-  -bams \
-    BAM_FILES/ATAC_1.bam \
-    BAM_FILES/ATAC_2.bam \
-    BAM_FILES/ATAC_n.bam \
-  -bed DIFFREPS/Roots_WT_All_diffRepsFC1.2.bed \
-  >DIFFREPS/ATAC_COMPARING_ROOTS_REGIONS_FILE.txt
