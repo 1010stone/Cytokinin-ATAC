f1551dc2cc3f9389959f6f9d4605316ece4cf7ab
Kevin Potter <potterkc@ad.unc.edu>
2018-11-16 09:44:11 -0500

    Delete Feature_Distribution_Plots.R

--- ATAC-Seq_analyses/Step4_Analyses_in_R/Feature_Distribution_Plots.R
+++
@@ -1,9 +0,0 @@
-#Plot a bargraph of different annotation features.
-
-plotAnnoBar(Roots_FC1.2_Anno) + theme(axis.text=element_text(size=18), 
-  axis.title=element_text(size=18), plot.title=element_text(size=18), 
-  legend.text=element_text(size = 12))
-
-plotAnnoBar(Shoots_FC1.2_Anno) + theme(axis.text=element_text(size=18), 
-  axis.title=element_text(size=18), plot.title=element_text(size=18), 
-  legend.text=element_text(size = 12))
