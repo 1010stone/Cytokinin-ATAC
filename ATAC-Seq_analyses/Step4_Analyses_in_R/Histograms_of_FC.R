7ef141c24f8f8cfaa5b07a8cbef1f2896a3f7b5b
Kevin Potter <potterkc@ad.unc.edu>
2018-11-16 09:02:59 -0500

    Create Histograms_of_FC.R

---
+++ ATAC-Seq_analyses/Step4_Analyses_in_R/Histograms_of_FC.R
@@ -0,0 +1,31 @@
+#Plotting histogram distribution of fold changes in roots
+Roots_FC1.2$absolute_log2FC = abs(Roots_FC1.2$log2FC)
+Shoots_FC1.2$absolute_log2FC = abs(Shoots_FC1.2$log2FC)
+
+Roots_FC1.2[mapply(is.infinite, 
+                       Roots_all_FC1.2)] <- NA
+Shoots_FC1.2[mapply(is.infinite, 
+                       Shoots_all_FC1.2)] <- NA
+
+mean(Roots_FC1.2$absolute_log2FC, rm.na = TRUE)
+mean(Shoots_FC1.2$absolute_log2FC, rm.na = TRUE)
+
+ggplot(Roots_FC1.2, aes(x = absolute_log2FC)) +
+  geom_histogram(binwidth = 0.05, 
+                 colour= "black", 
+                 fill = "red") + 
+  theme_classic() + 
+  geom_vline(aes(xintercept = mean(absolute_log2FC, 
+                                   rm.na = TRUE)), 
+             color = "black", linetype = "dashed", 
+             size = 1)
+
+ggplot(Shoots_FC1.2, aes(x = absolute_log2FC)) +
+  geom_histogram(binwidth = 0.05, 
+                 colour= "black", 
+                 fill = "red") + 
+  theme_classic() + 
+  geom_vline(aes(xintercept = mean(absolute_log2FC, 
+                                   rm.na = TRUE)), 
+             color = "black", linetype = "dashed", 
+             size = 1)  
